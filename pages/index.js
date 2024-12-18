a2616fa71c48bc457d6dba6a6ff2df7f232af674
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2024-12-18 23:44:58 +0900

    ぷｓｈ

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,20 @@ import FilterButton from '../components/FilterButton';
 import SearchBox from '../components/SearchBox';
 
 export default function Home() {
-  const [filterResults, setFilterResults] = useState([]);
-
-  const handleRankButtonClick = (results) => {
-    console.log('Filter results:', results);
-    setFilterResults(results);
+  const handleRankButtonClick = () => {
+    //後で実装
   };
 
   return (
     <div style={{ textAlign: 'center' }}>
       <h1 style={{ fontSize: '3em' }}>橋梁情報管理システム</h1>
       <SearchBox onSearch={(query) => console.log(query)} />
+      <div style={{ textAlign: 'right', marginTop: '20px' }}>
       <FilterButton column="Rank" value="Ⅰ" onResults={handleRankButtonClick} />
       <FilterButton column="Rank" value="Ⅱ" onResults={handleRankButtonClick} />
       <FilterButton column="Rank" value="Ⅲ" onResults={handleRankButtonClick} />
       <FilterButton column="Rank" value="Ⅳ" onResults={handleRankButtonClick} />
+      </div>
     </div>
   );
 }
