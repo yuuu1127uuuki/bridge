52ead1820633fe1d077765dee6c464e5b0c1d911
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2024-12-18 23:31:35 +0900

    nya

---
+++ components/SearchBox.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const SearchBox = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="ここに検索してEnter"
+        value={query}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        style={{ marginRight: '10px', width: '300px', height: '30px' }}
+      />
+      <button onClick={handleSearch} style={{ height: '36px' }}>検索</button>
+    </div>
+  );
+};
+
+export default SearchBox;
---
+++ pages/components.js
@@ -0,0 +1,52 @@
+import SearchBox from "../components/SearchBox";
+import Button from "../components/Button";
+import FilterButton from "../components/FilterButton"; // FilterButtonコンポーネントをインポート
+
+import { useState } from "react";
+
+export default function About() {
+  const [searchResult, setSearchResult] = useState('');
+  const handleSearch = (query) => {
+    console.log('Search query:', query);
+    setSearchResult(query); // 実際にはAPI呼び出しなどに利用
+  };
+
+  const handleButtonClick = () => {
+    console.log('Buttonがクリックされました');
+    // ボタンがクリックされたときの処理をここに追加
+  };
+
+  const handleFilterResults = (results) => {
+    console.log(results);
+  };
+
+  // components 内のコンポーネントを表示する
+  return (
+    <div>
+      <h1>コンポーネント一覧</h1>
+      <div style={{ padding: '16px' }}>
+        <h1>Search Box Example</h1>
+        <SearchBox onSearch={handleSearch} />
+        {searchResult && (
+          <p>
+            Search Result: <strong>{searchResult}</strong>
+          </p>
+        )}
+        <div style={{ padding: '16px' }}>
+          {/* Button コンポーネントを追加 */}
+          <h1>Button Example</h1>
+          <Button onClick={handleButtonClick} text="ボタン"></Button>
+        </div>
+        <div style={{ padding: '16px' }}>
+          {/* FilterButton コンポーネントを追加 */}
+          <h1>Filter Button Example</h1>
+          <FilterButton
+            column="Rank"
+            value="Ⅱ"
+            onResults={handleFilterResults}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
