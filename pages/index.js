bb891a8224e6311a8dbbbeb9260f10446cf2d893
yuuu1127uuuki <a079ffu@yamaguchi-u.ac.jp>
2024-12-25 12:21:43 +0900

    main page wo kaeta

--- pages/index.js
+++ pages/index.js
@@ -1,139 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import SearchBox from '../components/Atoms/SearchBox';
-import ResetButton from '../components/Molecules/ResetButton';
-import LogoutButton from '../components/Molecules/LogoutButton';
-import ConsoleWindow from '../components/Atoms/ConsoleWindow';
-import MapConponent from '../components/Atoms/MapComponent';
-import Button from '../components/Atoms/Button';
-import axios from 'axios';
-import RankButtons from '../components/Organisms/RankButtons';
-import YearButtons from '../components/Organisms/YeraButtons';
-import AddModal from '../components/Templates/AddModal';
-import EditModal from '../components/Templates/EditModal';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
-axios.defaults.baseURL = 'https://bridge-backend-6wcu.onrender.com';
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
 
-export default function Home() {
-  const [bridgedata, setBridgedata] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [selectedMarker, setSelectedMarker] = useState(null);
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get('/getopendata')
-      .then((response) => {
-        setBridgedata(response.data);
-        setFilteredData(response.data);
-      })
-      .catch((error) => {
-        console.error('データの取得に失敗しました', error);
-      });
-  }, []);
-
-  const handleSearch = (query) => {
-    const filtered = bridgedata.filter((item) => item.Name.includes(query));
-    setFilteredData(filtered);
-    if (filtered.length === 0) {
-      alert('該当するデータがありません');
-    }
-  };
-
-  const handleRankButtonClick = (onResults) => {
-    const filtered = onResults;
-    setFilteredData(filtered);
-    if (filtered.length === 0) {
-      alert('該当するデータがありません');
-    }
-  };
-
-  const handleMarkerClick = (item) => {
-    setSelectedMarker(item);
-  };
-
-  const handleDeleteButtonClick = async () => {
-    const isConfirmed = window.confirm('本当に削除しますか？');
-    if (!isConfirmed) return;
-
-    console.log('ニフラム');
-    try {
-      const response = await axios.delete(
-        `/deleteopendata/${selectedMarker._id}`,
-        {
-          method: 'DELETE',
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // 簡単なログイン処理
+        if (username === 'admin' && password === 'password') {
+            // ログイン成功時の処理
+            router.push('/main');
+        } else {
+            // ログイン失敗時の処理
+            alert('ユーザー名またはパスワードが違います。');
         }
-      );
-      alert('削除に成功しました');
-    } catch (error) {
-      console.error(error);
-      alert('削除中にエラーが発生しました');
-    }
-  };
+    };
 
-  const handleAddConfilmButtonClick = async (data) => {
-    const isConfirmed = window.confirm('本当に追加しますか？');
-    if (!isConfirmed) return;
-    console.log('なかまをよぶ');
-    try {
-      const response = await axios.post('/postopendata', data);
-      alert('追加に成功しました');
-      console.log(data); // 送信するデータを確認
-    } catch (error) {
-      console.error(error);
-      alert('追加中にエラーが発生しました');
-    }
-  };
-  const handleEditButtonClick = async (data) => {
-    try {
-      const response = await axios.put(
-        `/putopendata/${selectedMarker._id}`,
-        data
-      );
-      alert('更新に成功しました');
-      console.log(data);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-      alert('更新中にエラーが発生しました');
-    }
-  }
+    return (
+        <div style={styles.container}>
+            <h1 style={styles.title}>橋梁情報管理システム</h1>
+            <form onSubmit={handleLogin} style={styles.form}>
+                <div style={styles.inputGroup}>
+                    <label htmlFor="username" style={styles.label}>ユーザー名:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        style={styles.input}
+                    />
+                </div>
+                <div style={styles.inputGroup}>
+                    <label htmlFor="password" style={styles.label}>パスワード:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        style={styles.input}
+                    />
+                </div>
+                <button type="submit" style={styles.button}>ログイン</button>
+            </form>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div style={{ textAlign: 'center', backgroundColor: '#fdffe7' }}>
-        <h1 style={{ fontSize: '3em', color: '#8c7676', marginTop: '0px' }}>
-          橋梁情報管理システム
-        </h1>
-        <Button onClick={() => setIsAddModalOpen(true)} text="追加" />
-        <SearchBox onSearch={handleSearch} />
-        <div style={{ textAlign: 'right', marginTop: '20px' }}>
-          <RankButtons handleRankButtonClick={handleRankButtonClick} />
-        </div>
-        <ResetButton /><LogoutButton />
-        <YearButtons handleYearButtonClick={handleRankButtonClick} />
-        <div style={{ marginTop: '20px' }}>
-          <ConsoleWindow
-            data={selectedMarker}
-            onDelete={handleDeleteButtonClick}
-            onEdit={() => setIsEditModalOpen(true)}
-          />
-        </div>
-        <MapConponent data={filteredData} onMarkerClick={handleMarkerClick} />
-        <AddModal
-          isOpen={isAddModalOpen}
-          onClose={() => setIsAddModalOpen(false)}
-          onCancel={() => setIsAddModalOpen(false)}
-          onConfirm={(data) => handleAddConfilmButtonClick(data)}
-        />
-        <EditModal
-          isOpen={isEditModalOpen}
-          onClose={() => setIsEditModalOpen(false)}
-          onCancel={() => setIsEditModalOpen(false)}
-          onConfirm={(data) => handleEditButtonClick(data)}
-          editData={selectedMarker}
-        />
-      </div>
-    </>
-  );
-}
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#fdffe7',
+        color: '#8c7676',
+    },
+    title: {
+        marginBottom: '50px',
+        fontSize: '3em',
+        marginTop: '-50px',
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    inputGroup: {
+        marginBottom: '15px',
+    },
+    label: {
+        marginBottom: '5px',
+    },
+    input: {
+        padding: '10px',
+        borderRadius: '5px',
+        border: '1px solid #ccc',
+    },
+    button: {
+        padding: '10px 20px',
+        backgroundColor: '#c9fdd7',
+        color: '#8c7676',
+        border: 'none',
+        borderRadius: '5px',
+        outline: '1px solid #99f0ca',
+        cursor: 'pointer',
+    },
+};
+
+export default LoginPage;
