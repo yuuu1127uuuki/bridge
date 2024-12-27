e9bb9c372538e7f482dfa4782c552c89ff9a0265
Yuuki Yoshiura <133032958+yuuu1127uuuki@users.noreply.github.com>
2024-12-27 14:35:17 +0900

    Revert "akiyamechatta"

--- pages/history.js
+++ pages/history.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Home from './main';
-import HomeButton from '../components/Molecules/HomeButton';
 
 const History = () => {
     return (
         <div>
-            <h1>変更履歴確認</h1>
-            <HomeButton />
+            <h1>ぷくみん</h1>
         </div>
     );
 };
